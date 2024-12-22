3255d08e42ba6c0b42bb495cbffaecbc7ac9c315
kmhswimgirl <163170240+kmhswimgirl@users.noreply.github.com>
2024-12-22 13:33:05 -0500

    began making stubs for clock-display library

---
+++ src/clock-display.cpp
@@ -0,0 +1,35 @@
+#include <arduino.h>
+#include <clock-display.h>
+#include <iostream>
+
+//pin definitions for LEDS
+#define A1;
+#define A2;
+
+#define B1;
+#define B2;
+#define B3;
+#define B4;
+
+#define C1;
+#define C2;
+#define C3;
+
+#define D1;
+#define D2;
+#define D3;
+#define D4;
+
+//pin definitions for button input
+#define powerToggle;
+
+
+// void Display::displayTime(int){};
+
+// void Display:: loadingPhase(){};
+
+// void Display:: testLED(int column, int row){};
+
+bool Display::is24hr(){
+
+};
--- src/clock-display.h
+++ src/clock-display.h
@@ -1,7 +1,28 @@
+#ifndef BINARY_CLOCK
+#define BINARY_CLOCK
 #include <Arduino.h>
+#include <clock-display.cpp>
 
  
 //function that sets up the display for the time
-void binaryDigit(){
-    
-}
+class Display{
+    public:
+        //displays the current time on the clock
+        void displayTime(int);
+
+        // toggles if the display is using 24 hr or common time
+        bool is24hr();
+
+        //displays loading LED animation
+        void loadingPhase();
+
+        //test function to light certain LED
+        void testLED(int, int);
+
+
+    private:
+
+
+};
+
+#endif
